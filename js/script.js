5bc5a6a3157338cbc292ce438765cccc24253ccd
mafilika <Mafilikaholdingsandinnovation@gmail.com>
2026-03-02 15:07:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
-// LIGHTBOX
-const images = document.querySelectorAll('.gallery-grid img');
+// Mobile Menu
+const toggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('nav ul');
+
+if(toggle){
+toggle.addEventListener('click',()=>{
+navMenu.classList.toggle('active');
+});
+}
+
+// Lightbox
+const galleryImages = document.querySelectorAll('.gallery img');
 const lightbox = document.querySelector('.lightbox');
 const lightboxImg = document.querySelector('.lightbox img');
 
-images.forEach(img => {
-    img.addEventListener('click', () => {
-        lightbox.style.display = "flex";
-        lightboxImg.src = img.src;
-    });
+if(galleryImages){
+galleryImages.forEach(img=>{
+img.addEventListener('click',()=>{
+lightbox.style.display="flex";
+lightboxImg.src=img.src;
 });
-
-lightbox.addEventListener('click', () => {
-    lightbox.style.display = "none";
 });
+}
 
-// WHATSAPP FORM
-function sendToWhatsApp() {
-    const name = document.getElementById("name").value;
-    const service = document.getElementById("service").value;
-    const message = document.getElementById("message").value;
+if(lightbox){
+lightbox.addEventListener('click',()=>{
+lightbox.style.display="none";
+});
+}
 
-    const url = "https://wa.me/27736429105?text="
-        + "New Booking%0A"
-        + "Name: " + name + "%0A"
-        + "Service: " + service + "%0A"
-        + "Message: " + message;
+// WhatsApp Form
+function sendToWhatsApp(){
+let name=document.getElementById("name").value;
+let service=document.getElementById("service").value;
+let message=document.getElementById("message").value;
 
-    window.open(url, '_blank');
-      }
+let url="https://wa.me/27736429105?text="
++"NEW SERVICE BOOKING%0A%0A"
++"Name: "+name+"%0A"
++"Service Needed: "+service+"%0A"
++"Project Details: "+message+"%0A"
++"Location: Gauteng Province";
+
+window.open(url,'_blank');
+    }
